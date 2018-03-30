f41943a5aa1dec3b411b2272902b4e50173db764
punkyoon <cryptography@konkuk.ac.kr>
2018-03-30 18:48:05 +0000

    lib

--- as1/ku_ipc_lib.c
+++ as1/ku_ipc_lib.c
@@ -11,7 +11,6 @@ int ku_msgget(int key, int msgflg);
 int ku_msgclose(int msqid);
 int ku_msgsnd(int msqid, void *msgp, int msgsz, int msgflg);
 int ku_msgrcv(int msqid, void *msgp, int msgsz, long msgtyp, int msgflg);
-int ku_msgchk(int msqid);
 
 int ku_msgget(int key, int msgflg)
 {
@@ -75,9 +74,4 @@ int ku_msgrcv(int msgid, void *msgp, int msgsz, long msgtyp, int msgflg)
     int dev = open("/dev/ku_ipc_dev", O_RDWR);
     read(dev, msgp, msgsz);
     return 0;
-}
-
-int ku_msqchk(int msqid)
-{
-    return TRUE;
 }
